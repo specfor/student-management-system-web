21b7a88cbce9de8c983c344b7dbb193b5961c891
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-24 22:05:55 +0530

    page header fix company name showing

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { useSystemInfoStore } from '@/stores/systemInfo';
 import { UserCircleIcon } from '@heroicons/vue/24/solid'
+
+const systemInfoStore = useSystemInfoStore()
+let systemData = systemInfoStore.getInfo()
 </script>
 
 <template>
@@ -7,7 +11,7 @@ import { UserCircleIcon } from '@heroicons/vue/24/solid'
         <div class="flex py-2 px-8 items-center justify-between">
             <div class="flex gap-4">
                 <img src="/favicon.ico" alt="company logo">
-                <h4 class="font-semibold text-xl">Company Name</h4>
+                <h4 class="font-semibold text-xl">{{ systemData['company-name'] }}</h4>
             </div>
             <div class="flex">
                 <UserCircleIcon class="h-10 w-10" />
