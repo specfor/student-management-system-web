c6ed59cce627d77982306e8984e8c95536d3cb43
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-24 22:01:05 +0530

    basic routes configured

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppDashboard from '@/views/AppDashboard.vue'
 import UsersPage from '@/views/UsersPage.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/users',
       name: 'users',
       component: UsersPage
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
     }
     // {
     //   path: '/about',
