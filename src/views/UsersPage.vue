796a5e7d4b30e129fa579db42cb27ac78e4a6173
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-25 22:48:58 +0530

    basic user page setup is done

--- src/views/UsersPage.vue
+++ src/views/UsersPage.vue
@@ -1,6 +1,30 @@
 <script setup>
+import { sendGetRequest } from '@/baseFunctions/requests';
+
+import TableComponent from '@/components/TableComponent.vue';
+import { useAlertsStore } from '@/stores/alerts';
+import { ref } from 'vue';
+
+const alertStore = useAlertsStore()
+
+let userDataForTable = ref([])
+
+async function loadUsers() {
+    let data = await sendGetRequest('/users')
+    if (data.status === 'error') {
+        alertStore.insertAlert('An error occured.', data.message, 'error')
+    } else {
+        data.data.users.forEach(user => {
+            userDataForTable.value.push([user.id, user.name, user.email, user.role.role_name])
+        });
+    }
+}
+loadUsers()
 </script>
 
 <template>
-    <h4>Users</h4>
+    <div class="container">
+        <h4 class="font-semibold text-3xl mb-8">Users</h4>
+        <TableComponent :table-columns="['ID', 'Name', 'Email', 'Role']" :table-rows="userDataForTable" />
+    </div>
 </template>
