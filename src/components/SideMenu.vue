df5da08fbc87af516cd5e4f2af5ea32369b7d846
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-25 22:48:09 +0530

    Minor ui update

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,14 +1,20 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+
+
 </script>
 
 <template>
-    <div class="bg-back-secondary w-[200px] h-full">
-        <div class="flex py-2 px-4">
-            <RouterLink to="/">Dashboard</RouterLink>
-        </div>
-        <div class="flex py-2 px-4">
-            <RouterLink to="/users">Users</RouterLink>
-        </div>
+    <div class="bg-back-secondary w-[200px] pt-3">
+        <RouterLink to="/">
+            <div class="flex py-2 px-4 w-full hover:bg-gray-100">
+                Dashboard
+            </div>
+        </RouterLink>
+        <RouterLink to="/users">
+            <div class="flex py-2 px-4 w-full hover:bg-gray-100">
+                Users
+            </div>
+        </RouterLink>
     </div>
 </template>
