ed89a3ca7118fde625733169b110a972ab1d30aa
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-27 22:17:44 +0530

    new button icon added(use for main new item button in each page)

---
+++ src/components/minorUiComponents/NewItemButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+import { PlusCircleIcon } from '@heroicons/vue/24/solid';
+
+const { text, onClick } = defineProps(['text', 'onClick'])
+</script>
+<template>
+    <button class="font-semibold text-xl text-white" @click="onClick">
+        <div class="bg-blue-400 hover:bg-blue-600 py-1 px-6 rounded-lg flex items-center">
+            <PlusCircleIcon class="h-8 mr-3" /> {{ text }}
+        </div>
+    </button>
+</template>
