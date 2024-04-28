1055ed50fe16fe4e08a16bf5e3f9eccc0302fa7d
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-28 22:05:04 +0530

    menu update

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -6,15 +6,14 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <div class="bg-back-secondary w-[200px] pt-3">
-        <RouterLink to="/">
-            <div class="flex py-2 px-4 w-full hover:bg-gray-100">
-                Dashboard
-            </div>
+        <RouterLink to="/" class="flex py-2 px-4 w-full hover:bg-gray-100" active-class="bg-white">
+            Dashboard
         </RouterLink>
-        <RouterLink to="/users">
-            <div class="flex py-2 px-4 w-full hover:bg-gray-100">
-                Users
-            </div>
+        <RouterLink to="/users" class="flex py-2 px-4 w-full hover:bg-gray-100" active-class="bg-white">
+            Users
+        </RouterLink>
+        <RouterLink to="/user-roles" class="flex py-2 px-4 w-full hover:bg-gray-100" active-class="bg-white">
+            User Roles
         </RouterLink>
     </div>
 </template>
