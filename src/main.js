12015faac2e460bddd87f0513e48f581afffc73b
VIHANGA <vihanga2003nimsara@gmail.com>
2024-04-30 14:03:47 +0530

    user page basic functionality is completed

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import vClickOutside from 'click-outside-vue3'
+
 const app = createApp(App)
 
+app.use(vClickOutside)
 app.use(createPinia())
 app.use(router)
 
