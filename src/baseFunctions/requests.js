6b66d9200de4f2bef443996a3aa0dddc3231ed93
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-01 23:07:48 +0530

    added function to download files

--- src/baseFunctions/requests.js
+++ src/baseFunctions/requests.js
@@ -106,3 +106,35 @@ export async function sendJsonPatchRequest(url, jsonBody = {}, headers = {}) {
     return { status: 'error', message: 'Something went wrong.', data: null }
   }
 }
+
+export async function sendFileDownloadRequest(url, headers = {}) {
+  url = baseUrl + url
+  let headers_ = useHeaderStore().getHeaders()
+  headers_ = { ...headers_, ...headers }
+
+  let response = null
+  try {
+    response = await fetch(url, {
+      method: 'GET',
+      headers: headers_,
+      credentials: 'same-origin'
+    })
+  } catch (e) {
+    return { status: 'error', message: 'Failed to connect to the servers.', data: null }
+  }
+
+  if (response.status === 200)
+    return {
+      status: 'success',
+      data: { file: await response.blob() },
+      message: 'Succssfully downloaded the file.'
+    }
+
+  let data_
+  try {
+    data_ = await response.json()
+    return { status: data_.status, data: data_.data ?? data_.error, message: data_.message ?? null }
+  } catch (error) {
+    return { status: 'error', message: 'Something went wrong.', data: null }
+  }
+}
