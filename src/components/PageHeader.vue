3fe32aa63a09b4c2d6a6dcc1ad13a8a2026cc83a
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-16 11:57:05 +0530

    login page company icon not loading fix

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,10 +3,10 @@ import { useSystemInfoStore } from '@/stores/systemInfo';
 import { UserCircleIcon } from '@heroicons/vue/24/solid'
 import { ref } from 'vue'; ``
 import HeaderProfileIconDropdown from './HeaderProfileIconDropdown.vue'
+import { storeToRefs } from 'pinia';
 
 const systemInfoStore = useSystemInfoStore()
-let systemData = systemInfoStore.getInfo()
-
+let { sysInfo } = storeToRefs(systemInfoStore)
 const showProfileDropdown = ref(false)
 
 // function closeProfileDropdown(event) {
@@ -22,7 +22,7 @@ const showProfileDropdown = ref(false)
         <div class="flex py-2 px-8 items-center justify-between">
             <div class="flex gap-4 items-center">
                 <img src="/logo.png" alt="company logo" class="h-10">
-                <h4 class="font-semibold text-xl">{{ systemData['company-name'] }}</h4>
+                <h4 class="font-semibold text-xl">{{ sysInfo['company-name'] }}</h4>
             </div>
             <div class="flex">
                 <UserCircleIcon class="h-10 w-10 hover:bg-white rounded-md cursor-pointer"
