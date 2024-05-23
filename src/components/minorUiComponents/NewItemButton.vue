7c201f05486e0943ce6c40829f0ca7f824e227b9
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-23 21:10:36 +0530

    enrollment page updated to match new course structure

--- src/components/minorUiComponents/NewItemButton.vue
+++ src/components/minorUiComponents/NewItemButton.vue
@@ -5,9 +5,8 @@ import { PlusCircleIcon } from '@heroicons/vue/24/solid';
 const { text, onClick } = defineProps(['text', 'onClick'])
 </script>
 <template>
-    <button class="font-semibold text-xl text-white" @click="onClick">
-        <div class="bg-blue-400 hover:bg-blue-600 py-1 px-6 rounded-lg flex items-center">
-            <PlusCircleIcon class="h-8 mr-3" /> {{ text }}
-        </div>
+    <button class="font-semibold text-xl text-white disabled:cursor-not-allowed disabled:hover:bg-slate-500 disabled:bg-slate-500
+         bg-blue-400 hover:bg-blue-600 py-1 px-6 rounded-lg flex items-center " @click="onClick">
+        <PlusCircleIcon class="h-8 mr-3" /> {{ text }}
     </button>
 </template>
