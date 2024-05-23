b9cd617fd15c2234ccebd3942ab8f719ef94bd37
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-23 22:31:41 +0530

    updated error message functionality

--- src/components/formComponents/AddNewModal.vue
+++ src/components/formComponents/AddNewModal.vue
@@ -3,7 +3,7 @@
 import { useDataEntryFormsStore } from '@/stores/formManagers/dataEntryForm';
 import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { storeToRefs } from 'pinia';
-import { ArrowPathIcon } from '@heroicons/vue/24/solid';
+import { ArrowPathIcon, XCircleIcon } from '@heroicons/vue/24/solid';
 
 const dataEntryForm = useDataEntryFormsStore()
 const { show, previewUrls, submitted, allowSubmit, success, title, fields, fieldValues, successBtnText, errorMessages } = storeToRefs(dataEntryForm)
@@ -77,9 +77,11 @@ function handleFiles(fieldName, event) {
                         :key="index2">{{ option['text'] }}
                       </option>
                     </select>
-                    <div class="absolute top-7 right-0 z-[1000] w-2/3 px-2 rounded-lg bg-red-500/80 text-rose-900"
+                    <div class="mb-2 mt-1 px-2 flex items-center justify-between col-span-3 bg-red-300 text-red-900"
                       v-if="errorMessages[field['name']]">
-                      {{ errorMessages[field['name']] }}
+                      <h1>{{ errorMessages[field['name']] }}</h1>
+                      <XCircleIcon class="w-5 h-5 hover:fill-white"
+                        @click="dataEntryForm.removeErrorMessage(field['name'])" />
                     </div>
                   </div>
                   <div v-else-if="field['type'] === 'checkbox'" class="grid grid-cols-3 my-2 relative">
@@ -96,9 +98,11 @@ function handleFiles(fieldName, event) {
                           @input="event => fieldValues[field['name']][option['name']] = event.target.checked">
                         <p class="ml-2 font-semibold ">{{ option['text'] }}</p>
                       </div>
-                      <div class="absolute top-7 right-0 z-[1000] w-2/3 px-2 rounded-lg bg-red-500/80 text-rose-900"
+                      <div class="mb-2 mt-1 px-2 flex items-center justify-between col-span-3 bg-red-300 text-red-900"
                         v-if="errorMessages[field['name']]">
-                        {{ errorMessages[field['name']] }}
+                        <h1>{{ errorMessages[field['name']] }}</h1>
+                        <XCircleIcon class="w-5 h-5 hover:fill-white"
+                          @click="dataEntryForm.removeErrorMessage(field['name'])" />
                       </div>
                     </div>
                   </div>
@@ -113,9 +117,11 @@ function handleFiles(fieldName, event) {
                       @input="(event) => { fieldValues[field['name']] = event.target.value; validateInput(field['name']) }"
                       :disabled="field['disabled']" class="col-span-2 border-2 border-slate-400 rounded-md px-3 hover:border-slate-700
                                py-0.5 hover:bg-slate-100 focus:bg-slate-200 disabled:border"></textarea>
-                    <div class="absolute top-7 right-0 z-[1000] w-2/3 px-2 rounded-lg bg-red-500/80 text-rose-900"
+                    <div class="mb-2 mt-1 px-2 flex items-center justify-between col-span-3 bg-red-300 text-red-900"
                       v-if="errorMessages[field['name']]">
-                      {{ errorMessages[field['name']] }}
+                      <h1>{{ errorMessages[field['name']] }}</h1>
+                      <XCircleIcon class="w-5 h-5 hover:fill-white"
+                        @click="dataEntryForm.removeErrorMessage(field['name'])" />
                     </div>
                   </div>
                   <div v-else-if="field['type'] === 'message'" class="mt-3 mb-1">
@@ -146,6 +152,12 @@ function handleFiles(fieldName, event) {
                       <img v-show="field['preview'] && previewUrls[field['name']]" :src="previewUrls[field['name']]"
                         alt="selected file" class="w-auto mt-4 h-[300px] rounded-md">
                     </div>
+                    <div class="mb-2 mt-1 px-2 flex items-center justify-between col-span-3 bg-red-300 text-red-900"
+                      v-if="errorMessages[field['name']]">
+                      <h1>{{ errorMessages[field['name']] }}</h1>
+                      <XCircleIcon class="w-5 h-5 hover:fill-white"
+                        @click="dataEntryForm.removeErrorMessage(field['name'])" />
+                    </div>
                   </div>
                   <div v-else class="grid grid-cols-3 mb-1 relative">
                     <div class="font-semibold text-slate-700 py-0.5 flex justify-between">
@@ -156,9 +168,11 @@ function handleFiles(fieldName, event) {
                          py-0.5 hover:bg-slate-100 focus:bg-slate-200 disabled:border" :disabled="field['disabled']"
                       :type="field['type']" :value="fieldValues[field['name']]" :min="field['min']" :max="field['max']"
                       @input="(event) => { fieldValues[field['name']] = event.target.value; validateInput(field['name']) }">
-                    <div class="absolute top-7 right-0 z-[1000] w-2/3 px-2 rounded-b-lg bg-red-500/80 text-rose-900"
+                    <div class="mb-2 mt-1 px-2 flex items-center justify-between col-span-3 bg-red-300 text-red-900"
                       v-if="errorMessages[field['name']]">
-                      {{ errorMessages[field['name']] }}
+                      <h1>{{ errorMessages[field['name']] }}</h1>
+                      <XCircleIcon class="w-5 h-5 hover:fill-white"
+                        @click="dataEntryForm.removeErrorMessage(field['name'])" />
                     </div>
                   </div>
                 </div>
