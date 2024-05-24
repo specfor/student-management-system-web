cee2789aa39afce4fe512a7ca65f456d8fc402b5
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-24 14:50:44 +0530

    added a loading screen

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,35 @@ import { RouterView } from 'vue-router'
 import PageHeader from './components/PageHeader.vue';
 import SideMenu from './components/SideMenu.vue';
 import { useAuthStore } from './stores/authorization';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import router from './router';
 import { useSystemInfoStore } from './stores/systemInfo';
 import SideAlerts from './components/SideAlerts.vue';
 import AddNewModal from './components/formComponents/AddNewModal.vue';
 import ConfirmationModal from './components/formComponents/ConfirmationModal.vue';
 import ExtendablePopUp from './components/formComponents/ExtendablePopUp.vue';
+import LoadingScreen from '@/views/LoadingScreen.vue'
 
 const authStore = useAuthStore()
 useSystemInfoStore()
+const loading = ref(true)
 
 onMounted(async () => {
   await authStore.checkLoggedIn()
   if (authStore.LoggedIn) {
     if (router.currentRoute.value['path'] === '/login')
       router.push('/')
-  }
-
-  if (!authStore.LoggedIn) {
+  } else {
     router.push('/login')
   }
+  loading.value = false
 })
 
 </script>
 
 <template>
+  <LoadingScreen v-if="loading" />
+
   <div class="TextNotCopy">
     <div v-if="authStore.LoggedIn" class="h-dvh flex flex-col">
       <header>
