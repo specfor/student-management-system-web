60f106840a6be508c2c8c019c719a4ae97c077f5
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-24 14:50:44 +0530

    added a loading screen

---
+++ src/views/LoadingScreen.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-screen h-screen bg-white z-[500] fixed flex items-center justify-center">
+        <div class="flex flex-col items-center">
+            <div class="flex gap-2">
+                <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+                <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+                <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+            </div>
+            <h1 class="font-bold text-4xl mt-5">Initializing System.</h1>
+        </div>
+    </div>
+</template>
