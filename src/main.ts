aa7cbbaa72dabec6ca82ca485bc1a85291206930
VIHANGA <vihanga2003nimsara@gmail.com>
2024-06-14 15:38:56 +0530

    changed from js to ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import vClickOutside from 'click-outside-vue3'
 import { Tabs, Tab } from 'vue3-tabs-component'
 import Popper from 'vue3-popper'
 
@@ -14,7 +13,6 @@ const app = createApp(App).component('tabs', Tabs).component('tab', Tab)
 
 app.component('Popper', Popper)
 
-app.use(vClickOutside)
 app.use(createPinia())
 app.use(router)
 
