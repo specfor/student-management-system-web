ddaf858b2e7e9558c12a1a628d94e9a14666da6c
VIHANGA <vihanga2003nimsara@gmail.com>
2024-06-20 11:39:30 +0530

    made changes to enrollment type

--- src/components/customPopUps/StudentCourses.vue
+++ src/components/customPopUps/StudentCourses.vue
@@ -15,17 +15,17 @@ async function init(studentId: number) {
         return
     }
     (res.data.enrollments as Enrollment[]).forEach(enrollment => {
-        let cName = enrollment.course.name
-        let fee = Number(enrollment.course.fee.amount)
+        let cName = enrollment.course ? enrollment.course.name : 'Deleted Course'
+        let fee = Number(enrollment.course ? enrollment.course.fee.amount : 'Deleted Course')
         if (enrollment.price_adjustments != null) {
             if (enrollment.price_adjustments.type == 'fixed')
                 fee = fee - enrollment.price_adjustments.amount!
             else
                 fee = fee * (1 - enrollment.price_adjustments.percentage!)
         }
-        if (enrollment.course.group_name != null)
-            cName = cName + ' - ' + enrollment.course.group_name
-        courses.value.push([enrollment.course.id, cName, fee, enrollment.suspended ? 'Suspended' : 'Not Suspended'])
+        if (enrollment.course ? enrollment.course.group_name != null : 'Deleted Course')
+            cName = cName + ' - ' + (enrollment.course ? enrollment.course.group_name : 'Deleted Course')
+        courses.value.push([enrollment.course ? enrollment.course.id : 'Deleted Course', cName, fee, enrollment.suspended ? 'Suspended' : 'Not Suspended'])
     });
 }
 init(args.student.id)
