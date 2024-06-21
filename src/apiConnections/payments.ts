092ec18893033c00d8b2b8e261f401eeea3d2feb
VIHANGA <vihanga2003nimsara@gmail.com>
2024-06-21 17:46:56 +0530

    added byStudent and byCourse in payments

--- src/apiConnections/payments.ts
+++ src/apiConnections/payments.ts
@@ -1,8 +1,14 @@
 import { sendJsonPostRequest, sendGetRequest } from "@/baseFunctions/requests";
 
-export function getPayments(startIndex = 0, limit: null | number = null) {
+export function getPayments(
+  startIndex = 0,
+  limit: null | number = null,
+  filters: { course_id?: number; student_id?: number } = {}
+) {
   const params: { [key: string]: any } = { start: startIndex };
   if (limit) params["size"] = limit;
+  if (filters.course_id) params["course_id"] = filters.course_id;
+  if (filters.student_id) params["student_id"] = filters.student_id;
 
   return sendGetRequest("/payments", params);
 }
