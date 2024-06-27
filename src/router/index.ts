f599cac859394ae706f04feca496e6761a2787d0
VIHANGA <vihanga2003nimsara@gmail.com>
2024-06-27 10:06:00 +0530

    configured student more info into url base routing system

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,80 +1,86 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AppDashboard from '@/views/AppDashboard.vue'
-import UsersPage from '@/views/UsersPage.vue'
-import LoginPage from '@/views/LoginPage.vue'
-import UserRolesPage from '@/views/UserRolesPage.vue'
-import GradesPage from '@/views/GradesPage.vue'
-import StudentPage from '@/views/StudentPage.vue'
-import InstructorsPage from '@/views/InstructorsPage.vue'
-import CoursesPage from '@/views/CoursesPage.vue'
-import EnrollmentPage from '@/views/EnrollmentPage.vue'
-import PaymentsPage from '@/views/PaymentsPage.vue'
-import MarkAttendancePage from '@/views/MarkAttendancePage.vue'
-import AttendancePage from '@/views/AttendancePage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AppDashboard from "@/views/AppDashboard.vue";
+import UsersPage from "@/views/UsersPage.vue";
+import LoginPage from "@/views/LoginPage.vue";
+import UserRolesPage from "@/views/UserRolesPage.vue";
+import GradesPage from "@/views/GradesPage.vue";
+import StudentPage from "@/views/student/StudentPage.vue";
+import InstructorsPage from "@/views/InstructorsPage.vue";
+import CoursesPage from "@/views/CoursesPage.vue";
+import EnrollmentPage from "@/views/EnrollmentPage.vue";
+import PaymentsPage from "@/views/PaymentsPage.vue";
+import MarkAttendancePage from "@/views/MarkAttendancePage.vue";
+import AttendancePage from "@/views/AttendancePage.vue";
+import StudentMoreInfo from "@/views/student/StudentMoreInfo.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
-      component: AppDashboard
+      path: "/",
+      name: "dashboard",
+      component: AppDashboard,
     },
     {
-      path: '/users',
-      name: 'users',
-      component: UsersPage
+      path: "/users",
+      name: "users",
+      component: UsersPage,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginPage
+      path: "/login",
+      name: "login",
+      component: LoginPage,
     },
     {
-      path: '/user-roles',
-      name: 'user-roles',
-      component: UserRolesPage
+      path: "/user-roles",
+      name: "user-roles",
+      component: UserRolesPage,
     },
     {
-      path: '/grades',
-      name: 'grades',
-      component: GradesPage
+      path: "/grades",
+      name: "grades",
+      component: GradesPage,
     },
     {
-      path: '/students',
-      name: 'students',
-      component: StudentPage
+      path: "/students",
+      name: "students",
+      component: StudentPage,
     },
     {
-      path: '/instructors',
-      name: 'instructors',
-      component: InstructorsPage
+      path: "/students/:id/view",
+      name: "students-view",
+      component: StudentMoreInfo,
     },
     {
-      path: '/courses',
-      name: 'courses',
-      component: CoursesPage
+      path: "/instructors",
+      name: "instructors",
+      component: InstructorsPage,
     },
     {
-      path: '/enrollments',
-      name: 'enrollments',
-      component: EnrollmentPage
+      path: "/courses",
+      name: "courses",
+      component: CoursesPage,
     },
     {
-      path: '/payments',
-      name: 'payments',
-      component: PaymentsPage
+      path: "/enrollments",
+      name: "enrollments",
+      component: EnrollmentPage,
     },
     {
-      path: '/mark-attendance',
-      name: 'mark-attendance',
-      component: MarkAttendancePage
+      path: "/payments",
+      name: "payments",
+      component: PaymentsPage,
     },
     {
-      path: '/attendance',
-      name: 'attendance',
-      component: AttendancePage
-    }
+      path: "/mark-attendance",
+      name: "mark-attendance",
+      component: MarkAttendancePage,
+    },
+    {
+      path: "/attendance",
+      name: "attendance",
+      component: AttendancePage,
+    },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -83,7 +89,7 @@ const router = createRouter({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import('../views/AboutView.vue')
     // }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/student/StudentMoreInfo.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { downloadStudentImage, getStudentById, getStudents } from '@/apiConnections/students';
+import { useAlertsStore } from '@/stores/alerts';
+import { getRouterParam } from '@/utils/routeHelpers';
+import { ref, type Ref } from 'vue';
+
+const alertStore = useAlertsStore()
+
+const imageUrl: Ref<null | string> = ref(null)
+
+const studentId = getRouterParam('id')
+let student: Ref<Student | null> = ref(null)
+
+loadStudent(Number(studentId))
+loadImage(Number(studentId))
+
+async function loadStudent(studentID: number) {
+    let resp = await getStudentById(studentID)
+    if (resp.status === 'error') {
+        alertStore.insertAlert('An error occured.', resp.message, 'error')
+        return
+    }
+
+    student.value = resp.data.student
+}
+
+async function loadImage(studentId: number) {
+    let resp = await downloadStudentImage(studentId);
+    if (resp.status === 'error') {
+        imageUrl.value = '/default-profile.png'
+        return
+    }
+
+    imageUrl.value = URL.createObjectURL(resp.data.file)
+}
+</script>
+
+<template>
+    <div class="bg-slate-200 w-full">
+        <div class="container">
+            <div class="grid grid-cols-3 gap-4 p-5 rounded-xl w-fit bg-white">
+                <div class="flex flex-col items-center">
+                    <div v-if="imageUrl === null" class="flex items-center justify-center ml-3">
+                        <div
+                            class="animate-pulse bg-gray-400 w-[300px] h-[300px] rounded-lg flex items-center justify-center">
+                            <h4 class="text-2xl text-white">Loading</h4>
+                        </div>
+                    </div>
+                    <div v-else class="w-[300px] h-[300px] ml-3 flex items-center">
+                        <img :src="imageUrl" alt="student image" class="object-contain">
+                    </div>
+                    <!-- <button @click="() => { $emit('closeEmit'); args.uploadImageFunc(args.student.id); }"
+                class="border py-2 px-5 rounded-lg font-semibold mt-5">Update
+                Image</button> -->
+                </div>
+                <div v-if="student === null" class="flex items-center justify-center">
+                    <p>Loading</p>
+                </div>
+                <div v-else class="p-3 col-span-2 ml-5">
+                    <div class="grid grid-cols-3 items-center">
+                        <h4>ID</h4>
+                        <p class="col-span-2 border-b border-slate-300 ">{{ student.id }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Student ID</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.custom_id ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Name</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.name ?? 'None' }}
+                        </p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Full Name</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.full_name ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Email</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.email ?? 'None' }}
+                        </p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Phone Number</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.phone_number ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Birthday</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.birthday ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Grade</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.grade ? student.grade.name : "None"
+                            }}
+                        </p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Address</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.address ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>School</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.school ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Parent's Name</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.parent_name ?? 'None' }}</p>
+                    </div>
+                    <div class="grid grid-cols-3 mt-3 items-center">
+                        <h4>Parent's Phone Number</h4>
+                        <p class="col-span-2 border-b border-slate-300">{{ student.parent_phone_number ?? 'None' }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
