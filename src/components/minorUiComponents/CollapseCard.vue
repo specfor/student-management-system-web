8a8322b329da22b0d859448abb68352b3553eae4
VIHANGA <vihanga2003nimsara@gmail.com>
2024-07-22 21:30:26 +0530

    added active student count, dashboad items

---
+++ src/components/minorUiComponents/CollapseCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/24/solid';
+import { ref } from 'vue';
+
+let { header } = defineProps<{
+    header: string
+}>()
+
+const expanded = ref(true);
+
+function toggleExpand() {
+    expanded.value = !expanded.value
+}
+</script>
+
+<template>
+    <div class="border-2  rounded-lg bg-white h-fit">
+        <div class="px-4 py-2 flex justify-between">
+            <h4 class="font-semibold text-lg">{{ header }}</h4>
+            <ChevronUpIcon v-show="expanded" @click="toggleExpand" class="w-8 h-8 cursor-pointer" />
+            <ChevronDownIcon v-show="!expanded" @click="toggleExpand" class="w-8 h-8 cursor-pointer" />
+        </div>
+        <div v-show="expanded" class="border-t px-4 py-5">
+            <slot></slot>
+        </div>
+
+    </div>
+</template>
--- src/views/AppDashboard.vue
+++ src/views/AppDashboard.vue
@@ -1,8 +1,113 @@
 <script setup lang="ts">
+import { getMonthlyIncomeSummary, getStudentCount } from '@/apiConnections/analytics';
+import CollapseCard from '@/components/minorUiComponents/CollapseCard.vue';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { ref, type Ref } from 'vue';
+import { Doughnut } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const studentCountData: Ref<{
+    labels: string[];
+    datasets: {
+        backgroundColor: string[],
+        data: number[]
+    }[]
+}> = ref({
+    labels: ['Active', 'Inactive'],
+    datasets: [
+        {
+            backgroundColor: ['#00D8FF', '#FF6A19'],
+            data: []
+        }
+    ]
+})
+
+const incomeDataForGraph: Ref<{
+    labels: string[];
+    datasets: {
+        backgroundColor: string[],
+        data: number[]
+    }[]
+}> = ref({
+    labels: ['As This Month Class Fees', 'For Out-Standing Class Payments'],
+    datasets: [
+        {
+            backgroundColor: ['#45E713', '#AF19FF'],
+            data: []
+        }
+    ]
+})
+
+const incomeData: Ref<{
+    total_income?: { amount: string, currency: string },
+    this_month_class_payments?: { amount: string, currency: string },
+    out_standing_class_payments?: { amount: string, currency: string },
+}> = ref({})
+
+
+async function loadStudentCount() {
+    let resp = await getStudentCount()
+    if (resp.status === 'success') {
+        studentCountData.value.datasets[0].data = [resp.data.active, resp.data.inactive]
+    }
+}
+loadStudentCount()
+
+async function loadMonthlyIncomeSummary() {
+    let resp = await getMonthlyIncomeSummary(new Date().getFullYear(), new Date().getMonth())
+    if (resp.status === 'success') {
+        incomeData.value = resp.data
+        incomeDataForGraph.value.datasets[0].data = [resp.data.this_month_class_payments.amount, resp.data.out_standing_class_payments.amount]
+    }
+}
+loadMonthlyIncomeSummary()
+
+function formatMoney(amount: string) {
+    let m = amount.split('.')
+    let final = ""
+
+    final += m[0].substring(0, m[0].length % 3) + ','
+
+    for (let i = m[0].length % 3; i < m[0].length; i += 3) {
+        final += m[0].substring(i, i + 3)
+        if (i !== m[0].length - 3)
+            final += ','
+    }
+    return final
+}
 </script>
 
 <template>
-    <h4>Dashboard</h4>
+    <div class="bg-slate-200 w-full">
+        <div class="container">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                <CollapseCard :header="'Monthly Income - ' + months[new Date().getMonth()]">
+                    <div class="w-full h-300px">
+                        <Doughnut :data="incomeDataForGraph" :options="{ responsive: true, maintainAspectRatio: false }"
+                            v-if="incomeDataForGraph.datasets[0].data.length > 0" />
+                    </div>
+                    <div class="flex justify-center gap-x-10 mt-8" v-if="incomeData.total_income">
+                        <h5>Total Income</h5>
+                        <p>{{ incomeData.total_income.currency }} {{ formatMoney(incomeData.total_income.amount) }}</p>
+                    </div>
+                </CollapseCard>
+
+                <CollapseCard header="Student Summary">
+                    <div class="w-full h-300px">
+                        <Doughnut :data="studentCountData" :options="{ responsive: true, maintainAspectRatio: false }"
+                            v-if="studentCountData.datasets[0].data.length > 0" />
+                    </div>
+                    <div class="flex justify-center gap-x-10 mt-8" v-if="studentCountData.datasets[0].data.length > 0">
+                        <h5>Total Number of Students</h5>
+                        <p>{{ studentCountData.datasets[0].data[0] + studentCountData.datasets[0].data[1] }}</p>
+                    </div>
+                </CollapseCard>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
