cc39ea97e7bb7907acb68d1347f49f1d9bfb5c18
VIHANGA <vihanga2003nimsara@gmail.com>
2024-07-26 23:12:24 +0530

    added instructor monthly income calculator

---
+++ src/types/analytics.ts
@@ -0,0 +1,42 @@
+type AnalyticsInstructorMonthlyPaymentCalculation = {
+  overall: {
+    payments: {
+      total: APIMoney;
+      instructor_share: APIMoney;
+      class_share: APIMoney;
+    };
+  };
+  for_selected_month: AnalyticsPaymentRecord[];
+  for_other_months: AnalyticsPaymentRecord[];
+  relation_data: {
+    students: {
+      id: number;
+      name: string;
+      custom_id: string | null;
+    }[];
+  };
+};
+
+type AnalyticsPaymentRecord = {
+  course_id: number;
+  course_name: string;
+  course_fee: CourseFee;
+  tot_payments: APIMoney;
+  instructor_share: APIMoney;
+  class_share: APIMoney;
+  instructor_fee_percentage: number;
+  enrollment_count: number;
+  records: {
+    student_id: number;
+    attended_day_count: number;
+    payment_amount: string;
+    instructor_share: APIMoney;
+    class_share: APIMoney;
+    month: string;
+  }[];
+};
+
+type APIMoney = {
+  amount: string;
+  currency: string;
+};
