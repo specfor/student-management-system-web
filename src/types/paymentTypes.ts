6271a070bdb3cf95b2711aec87c8724d8138f151
VIHANGA <vihanga2003nimsara@gmail.com>
2024-08-04 01:23:58 +0530

    added instructor payment routes and types

--- src/types/paymentTypes.ts
+++ src/types/paymentTypes.ts
@@ -9,3 +9,56 @@ type StudentPayment = {
   updated_at: string;
   enrollment: Enrollment;
 };
+
+// used in instructor payments calculation and things
+
+type InstructorPayment = {
+  id: number;
+  instructor_id: number;
+  paid_month: string;
+  tot_amount: APIMoney;
+  tot_pending_amount: APIMoney;
+  pending_payments_resolved: boolean;
+  enrollment_ids_for_pending_payments: null | number[];
+  payment_ids: null | number[];
+  created_at: string;
+  updated_at: string;
+  instructor: Instructor;
+};
+
+type StudentPaymentListItem = {
+  student_id: number;
+  attended_day_count: number;
+  payment_amount: APIMoney;
+  payment_id: number | null;
+  enrollment_id: number | null;
+  paid_date: string | null;
+  paid_instructor_share: boolean;
+  month: string;
+};
+
+type StudentPaymentListingResponse = {
+  for_selected_month: {
+    course_id: number;
+    course_name: string;
+    course_fee: CourseFee;
+    instructor_fee_percentage: number;
+    enrollment_count: number;
+    records: StudentPaymentListItem[];
+  }[];
+  for_other_months: {
+    course_id: number;
+    course_name: string;
+    course_fee: CourseFee;
+    instructor_fee_percentage: number;
+    enrollment_count: number;
+    records: StudentPaymentListItem[];
+  }[];
+  relation_data: {
+    students: {
+      id: number;
+      name: string;
+      custom_id: string;
+    }[];
+  };
+};
