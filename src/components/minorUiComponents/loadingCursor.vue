f880ca0bad9a5d8e001622d5dd867bc95e4a45d0
VIHANGA <vihanga2003nimsara@gmail.com>
2024-08-14 16:46:46 +0530

    basic loading component added

---
+++ src/components/minorUiComponents/loadingCursor.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full h-full flex justify-center items-center">
+        <div class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-700"></div>
+    </div>
+</template>
