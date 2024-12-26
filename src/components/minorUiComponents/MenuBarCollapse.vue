511af3af02017897a146f4bde75408abbe4a8abc
VIHANGA <vihanga2003nimsara@gmail.com>
2024-12-26 12:19:00 +0530

    added expand and collapse feature

--- src/components/minorUiComponents/MenuBarCollapse.vue
+++ src/components/minorUiComponents/MenuBarCollapse.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/24/outline';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 let expanded = ref(false);
+
+const props = defineProps<{
+    options?: {
+        hideExpandCollapseIcons?: boolean
+    }
+}>()
+
+watch(props, (val) => {
+    if (val.options?.hideExpandCollapseIcons)
+        expanded.value = false
+})
 </script>
 
 <template>
@@ -10,8 +21,10 @@ let expanded = ref(false);
         <div class="flex items-center justify-between cursor-pointer  hover:bg-blue-700" @click="expanded = !expanded">
             <slot name="header">
             </slot>
-            <ChevronUpIcon class="w-6 h-6 stroke-2 mr-2 text-white" v-show="expanded" />
-            <ChevronDownIcon class="w-6 h-6 stroke-2 mr-2 text-white" v-show="!expanded" />
+            <ChevronUpIcon class="w-6 h-6 stroke-2 mr-2 text-white"
+                v-show="expanded && !props.options?.hideExpandCollapseIcons" />
+            <ChevronDownIcon class="w-6 h-6 stroke-2 mr-2 text-white"
+                v-show="!expanded && !props.options?.hideExpandCollapseIcons" />
         </div>
 
         <div v-show="expanded" class="text-sm">
