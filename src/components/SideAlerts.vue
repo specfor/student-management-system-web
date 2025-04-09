8da63e1de86dfd93167e18ba23ff86cf4a087819
VIHANGA <vihanga2003nimsara@gmail.com>
2025-04-09 21:51:41 +0530

    added info alert type

--- src/components/SideAlerts.vue
+++ src/components/SideAlerts.vue
@@ -13,7 +13,8 @@ function removeNotification(id: number) {
   <div class="space-y-3 items-end flex flex-col">
     <TransitionGroup name="notifications">
       <div v-for="notification in alertStore.alerts" :key="notification['id']">
-        <div v-if="notification['type'] === 'success'" class="flex p-2 bg-green-400/90 rounded-lg space-x-5 w-max">
+        <div v-if="notification['type'] === 'success'"
+          class="flex p-2 bg-green-400/90 hover:bg-green-400 rounded-lg space-x-5 w-max">
           <CheckCircleIcon class="fill-green-800 w-[50px] h-[50px]" />
           <div>
             <div class="text-xl font-semibold text-green-900">{{ notification['title'] }}</div>
@@ -23,7 +24,8 @@ function removeNotification(id: number) {
             class="stroke-green-800 stroke-2 w-[25px] h-[25px] hover:border-2 hover:border-green-600 mt-1 justify-self-end"
             @click="removeNotification(notification['id'])" />
         </div>
-        <div v-else-if="notification['type'] === 'error'" class="flex p-2 bg-rose-400/90 rounded-lg space-x-5 w-max">
+        <div v-else-if="notification['type'] === 'error'"
+          class="flex p-2 bg-rose-400/90 hover:bg-rose-400 rounded-lg space-x-5 w-max">
           <ExclamationCircleIcon class="fill-rose-800 w-[50px] h-[50px]" />
           <div>
             <div class="text-xl font-semibold text-rose-900">{{ notification['title'] }}</div>
@@ -31,6 +33,17 @@ function removeNotification(id: number) {
           </div>
           <XMarkIcon
             class="stroke-rose-800 stroke-2 w-[25px] h-[25px] hover:border-2 hover:border-rose-600 mt-1 justify-self-end"
+            @click="removeNotification(notification['id'])" />
+        </div>
+        <div v-else-if="notification['type'] === 'info'"
+          class="flex p-2 bg-blue-400/90 hover:bg-blue-400 rounded-lg space-x-5 w-max">
+          <ExclamationCircleIcon class="fill-blue-800 w-[50px] h-[50px]" />
+          <div>
+            <div class="text-xl font-semibold text-blue-900">{{ notification['title'] }}</div>
+            <div class="text-blue-900">{{ notification['message'] }}</div>
+          </div>
+          <XMarkIcon
+            class="stroke-blue-800 stroke-2 w-[25px] h-[25px] hover:border-2 hover:border-blue-600 mt-1 justify-self-end"
             @click="removeNotification(notification['id'])" />
         </div>
       </div>
