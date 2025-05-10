5f2528ca86a3347c05def96f7dc778f3465ccea7
VIHANGA <vihanga2003nimsara@gmail.com>
2025-05-10 12:59:55 +0530

    added option to collapse on show

--- src/components/minorUiComponents/CollapseCard.vue
+++ src/components/minorUiComponents/CollapseCard.vue
@@ -2,13 +2,18 @@
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/24/solid';
 import { ref } from 'vue';
 
-let { header } = defineProps<{
+let { header, collapseOnStart } = defineProps<{
     header: string
     headerTextCss?: string
     headerCss?: string
+    collapseOnStart?: boolean
 }>()
 
 const expanded = ref(true);
+
+if (collapseOnStart) {
+    expanded.value = false
+}
 
 function toggleExpand() {
     expanded.value = !expanded.value
@@ -19,8 +24,10 @@ function toggleExpand() {
     <div class="border-2  rounded-lg bg-white h-fit">
         <div class="px-4 py-2 flex justify-between" :class="headerCss">
             <h4 :class="headerTextCss ? headerTextCss : 'font-semibold text-lg'">{{ header }}</h4>
-            <ChevronUpIcon v-show="expanded" @click="toggleExpand" class="w-8 h-8 cursor-pointer" />
-            <ChevronDownIcon v-show="!expanded" @click="toggleExpand" class="w-8 h-8 cursor-pointer" />
+            <ChevronUpIcon v-show="expanded" @click="toggleExpand"
+                class="w-8 h-8 cursor-pointer hover:bg-slate-300 rounded-full" />
+            <ChevronDownIcon v-show="!expanded" @click="toggleExpand"
+                class="w-8 h-8 cursor-pointer hover:bg-slate-300 rounded-full" />
         </div>
         <div v-show="expanded" class="border-t px-4 py-5">
             <slot></slot>
