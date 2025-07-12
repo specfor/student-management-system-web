f385973c9e6ad344df16922119a728377e17fad8
Vihanga Nimsara <vihanga2003nimsara@gmail.com>
2025-07-12 10:29:23 +0530

    prettier reformatting

--- src/views/settings/AttendanceMarker.vue
+++ src/views/settings/AttendanceMarker.vue
@@ -1,25 +1,32 @@
 <!-- eslint-disable no-constant-condition -->
 <script setup lang="ts">
-import { createBanner, deleteBanner, downloadBannerImage, getClientBanners, updateBanner, uploadBannerImage } from '@/apiConnections/client-banners';
-import CollapseCard from '@/components/minorUiComponents/CollapseCard.vue';
-import LoadingCursor from '@/components/minorUiComponents/loadingCursor.vue';
-import NewItemButton from '@/components/minorUiComponents/NewItemButton.vue';
-import PaginateComponent from '@/components/PaginateComponent.vue';
-import { useAlertsStore } from '@/stores/alerts';
-import { useConfirmationFormsStore } from '@/stores/formManagers/confirmationForm';
-import { useDataEntryFormsStore } from '@/stores/formManagers/dataEntryForm';
-import type { ClientBanner } from '@/types/client-banners';
-import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/solid';
-import { ref, type Ref } from 'vue';
+import {
+    createBanner,
+    deleteBanner,
+    downloadBannerImage,
+    getClientBanners,
+    updateBanner,
+    uploadBannerImage,
+} from "@/apiConnections/client-banners";
+import CollapseCard from "@/components/minorUiComponents/CollapseCard.vue";
+import LoadingCursor from "@/components/minorUiComponents/loadingCursor.vue";
+import NewItemButton from "@/components/minorUiComponents/NewItemButton.vue";
+import PaginateComponent from "@/components/PaginateComponent.vue";
+import { useAlertsStore } from "@/stores/alerts";
+import { useConfirmationFormsStore } from "@/stores/formManagers/confirmationForm";
+import { useDataEntryFormsStore } from "@/stores/formManagers/dataEntryForm";
+import type { ClientBanner } from "@/types/client-banners";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/24/solid";
+import { ref, type Ref } from "vue";
 
 const alertStore = useAlertsStore();
-const dataEntryForm = useDataEntryFormsStore()
-const confirmationForm = useConfirmationFormsStore()
+const dataEntryForm = useDataEntryFormsStore();
+const confirmationForm = useConfirmationFormsStore();
 
 const bannerData: Ref<ClientBanner[]> = ref([]);
 const isLoading = ref(false);
 
-const pageSize = 20;
+const itemsPerPage = 3;
 const totalCount = ref(0);
 const currentPage = ref(1);
 
@@ -27,11 +34,11 @@ const downloadedImages: Ref<Map<string, string>> = ref(new Map());
 
 async function fetchBanners() {
     isLoading.value = true;
-    const startIndex = currentPage.value * pageSize - pageSize;
-    const response = await getClientBanners(startIndex, pageSize);
+    const startIndex = currentPage.value * itemsPerPage - itemsPerPage;
+    const response = await getClientBanners(startIndex, itemsPerPage);
 
-    if (response.status == 'error') {
-        alertStore.insertAlert('Data fetch error', 'Unable to fetch data from server.', 'error');
+    if (response.status == "error") {
+        alertStore.insertAlert("Data fetch error", "Unable to fetch data from server.", "error");
     } else {
         bannerData.value = response.data.banners;
         bannerData.value.forEach((banner) => {
@@ -49,8 +56,8 @@ async function downloadImage(bannerId: number, imageId: string) {
         return;
     } else {
         const response = await downloadBannerImage(bannerId);
-        if (response.status == 'error') {
-            alertStore.insertAlert('Image download error', 'Unable to download image.', 'error');
+        if (response.status == "error") {
+            alertStore.insertAlert("Image download error", "Unable to download image.", "error");
             return;
         } else {
             const imageUrl = URL.createObjectURL(response.data.file);
@@ -62,107 +69,107 @@ async function downloadImage(bannerId: number, imageId: string) {
 async function addNewBanner() {
     dataEntryForm.newDataEntryForm("Add New Banner", "Create", [
         {
-            name: 'seconds_to_show',
-            type: 'number',
-            text: 'Seconds to show',
+            name: "seconds_to_show",
+            type: "number",
+            text: "Seconds to show",
             required: true,
         },
         {
-            name: 'image',
-            type: 'file',
-            text: 'Banner Image',
+            name: "image",
+            type: "file",
+            text: "Banner Image",
             preview: true,
-            accept: 'image/*',
+            accept: "image/*",
             required: true,
         },
-    ])
+    ]);
 
     while (true) {
-        const results = await dataEntryForm.waitForSubmittedData()
-        if (!results.submitted)
-            return
+        const results = await dataEntryForm.waitForSubmittedData();
+        if (!results.submitted) return;
 
-        const imageFile = (results.data['image'] as any)[0];
+        const imageFile = (results.data["image"] as any)[0];
 
-        let uploadResp = await uploadBannerImage(imageFile)
-        console.log(uploadResp);
-        if (uploadResp.status == 'error') {
-            alertStore.insertAlert('Image upload error', 'Unable to upload image.', 'error');
+        let uploadResp = await uploadBannerImage(imageFile);
+
+        if (uploadResp.status == "error") {
+            alertStore.insertAlert("Image upload error", "Unable to upload image.", "error");
             return;
         } else {
             const imageId = uploadResp.data.image;
-            const secondsToShow = results.data['seconds_to_show'];
+            const secondsToShow = results.data["seconds_to_show"];
             const createResp = await createBanner(imageId, secondsToShow as number);
-            if (createResp.status == 'error') {
-                alertStore.insertAlert('Banner creation error', 'Unable to create banner.', 'error');
+            if (createResp.status == "error") {
+                alertStore.insertAlert("Banner creation error", "Unable to create banner.", "error");
             } else {
-                alertStore.insertAlert('Banner created', 'New banner created successfully.', 'success');
+                alertStore.insertAlert("Banner created", "New banner created successfully.", "success");
                 fetchBanners();
-                dataEntryForm.finishSubmission()
+                dataEntryForm.finishSubmission();
                 return;
             }
         }
-
     }
 }
 
 async function editBanner(bannerId: number) {
-    const banner = bannerData.value.find(b => b.id == bannerId);
+    const banner = bannerData.value.find((b) => b.id == bannerId);
     if (!banner) {
-        alertStore.insertAlert('Banner not found', 'Unable to find the specified banner.', 'error');
+        alertStore.insertAlert("Banner not found", "Unable to find the specified banner.", "error");
         return;
     }
 
     dataEntryForm.newDataEntryForm("Edit Banner", "Update", [
         {
-            name: 'seconds_to_show',
-            type: 'number',
-            text: 'Seconds to show',
+            name: "seconds_to_show",
+            type: "number",
+            text: "Seconds to show",
             required: true,
             value: banner.seconds_to_show,
         },
         {
-            name: 'active',
-            type: 'select',
+            name: "active",
+            type: "select",
             text: "Status",
             required: true,
             options: [
-                { value: 'published', text: "Published" },
-                { value: 'draft', text: "Draft" },
+                { value: "published", text: "Published" },
+                { value: "draft", text: "Draft" },
             ],
-            value: banner.active ? 'published' : 'draft',
+            value: banner.active ? "published" : "draft",
         },
-    ])
+    ]);
 
     while (true) {
-        const results = await dataEntryForm.waitForSubmittedData()
-        if (!results.submitted)
-            return
+        const results = await dataEntryForm.waitForSubmittedData();
+        if (!results.submitted) return;
 
-        const secondsToShow = results.data['seconds_to_show'];
-        const createResp = await updateBanner(bannerId, secondsToShow as number, results.data['active'] == 'published');
-        if (createResp.status == 'error') {
-            alertStore.insertAlert('Banner update error', 'Unable to update banner.', 'error');
+        const secondsToShow = results.data["seconds_to_show"];
+        const createResp = await updateBanner(bannerId, secondsToShow as number, results.data["active"] == "published");
+        if (createResp.status == "error") {
+            alertStore.insertAlert("Banner update error", "Unable to update banner.", "error");
         } else {
-            alertStore.insertAlert('Banner updated', 'Banner updated successfully.', 'success');
+            alertStore.insertAlert("Banner updated", "Banner updated successfully.", "success");
             fetchBanners();
-            dataEntryForm.finishSubmission()
+            dataEntryForm.finishSubmission();
             return;
         }
     }
 }
 
 async function removeBanner(bannerId: number) {
-    let confirmed = await confirmationForm.newConfirmationForm("Delete Banner", "Are you sure you want to delete this banner?");
+    let confirmed = await confirmationForm.newConfirmationForm(
+        "Delete Banner",
+        "Are you sure you want to delete this banner?"
+    );
     if (!confirmed) {
         return;
     }
 
     const response = await deleteBanner(bannerId);
-    if (response.status == 'error') {
-        alertStore.insertAlert('Banner deletion error', 'Unable to delete banner.', 'error');
+    if (response.status == "error") {
+        alertStore.insertAlert("Banner deletion error", "Unable to delete banner.", "error");
     } else {
-        alertStore.insertAlert('Banner deleted', 'Banner deleted successfully.', 'success');
+        alertStore.insertAlert("Banner deleted", "Banner deleted successfully.", "success");
         fetchBanners();
     }
 }
@@ -179,7 +186,6 @@ fetchBanners();
                     <NewItemButton @click="addNewBanner" text="Add New Banner" />
                 </div>
 
-
                 <div v-if="isLoading" class="text-center">
                     <LoadingCursor />
                 </div>
@@ -187,29 +193,57 @@ fetchBanners();
                 <div v-else>
                     <div class="grid grid-cols-4 gap-5">
                         <div v-for="banner in bannerData" :key="banner.id" class="border w-full mb-2 relative">
-                            <p class="absolute top-5 left-5 px-3 py-2 text-sm rounded-md text-white font-semibold z-10"
-                                :class="banner.active ? 'bg-green-500' : 'bg-yellow-500'">
-                                {{ banner.active ? "Published" : "Draft" }}</p>
+                            <p
+                                class="absolute top-5 left-5 px-3 py-2 text-sm rounded-md text-white font-semibold z-10"
+                                :class="banner.active ? 'bg-green-500' : 'bg-yellow-500'"
+                            >
+                                {{ banner.active ? "Published" : "Draft" }}
+                            </p>
                             <div>
-                                <div v-show="!downloadedImages.get(banner.image)"
-                                    class="w-full h-[200px] bg-gray-300 animate-pulse"></div>
-                                <img v-show="downloadedImages.get(banner.image)"
-                                    :src="downloadedImages.get(banner.image)" alt="Banner Image"
-                                    class="w-full h-[200px] object-fill object-center" />
+                                <div
+                                    v-show="!downloadedImages.get(banner.image)"
+                                    class="w-full h-[200px] bg-gray-300 animate-pulse"
+                                ></div>
+                                <img
+                                    v-show="downloadedImages.get(banner.image)"
+                                    :src="downloadedImages.get(banner.image)"
+                                    alt="Banner Image"
+                                    class="w-full h-[200px] object-fill object-center"
+                                />
                             </div>
                             <div class="flex justify-end mt-2 mr-5">
                                 <PencilSquareIcon
                                     class="h-6 w-6 fill-blue-500 active:fill-blue-700 mr-2 hover:cursor-pointer"
-                                    @click="() => { editBanner(banner.id) }" />
-                                <TrashIcon class="h-6 w-6 fill-red-500 active:fill-red-700 hover:cursor-pointer"
-                                    @click="() => { removeBanner(banner.id) }" />
+                                    @click="
+                                        () => {
+                                            editBanner(banner.id);
+                                        }
+                                    "
+                                />
+                                <TrashIcon
+                                    class="h-6 w-6 fill-red-500 active:fill-red-700 hover:cursor-pointer"
+                                    @click="
+                                        () => {
+                                            removeBanner(banner.id);
+                                        }
+                                    "
+                                />
                             </div>
                             <p class="px-4 my-3">Seconds Shown - {{ banner.seconds_to_show }}</p>
                         </div>
                     </div>
-                    <PaginateComponent :page-size="pageSize" :total-count="totalCount" @load-page-emit="(startIndex) => {
-                        currentPage = ((startIndex / (pageSize ?? 30)) | 0) + 1; fetchBanners();
-                    }" class="mt-5" />
+                    <PaginateComponent
+                        :page-size="itemsPerPage"
+                        :total-count="totalCount"
+                        @load-page-emit="
+                            (startIndex) => {
+                                currentPage = ((startIndex / (itemsPerPage ?? 30)) | 0) + 1;
+                                fetchBanners();
+                            }
+                        "
+                        :current-page="currentPage"
+                        class="mt-5"
+                    />
                 </div>
             </CollapseCard>
         </div>
